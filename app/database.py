b9a0638190e43d1a2d793a1df4c322d0b515922f
erlynpino <erlynpino@gmail.com>
2026-05-24 19:46:35 -0500

    fix: skip pool_size/max_overflow for SQLite engine

--- app/database.py
+++ app/database.py
@@ -4,13 +4,15 @@ from sqlmodel import Session, create_engine
 
 from app.config import settings
 
-engine = create_engine(
-    settings.database_url,
-    echo=False,
-    pool_pre_ping=True,
-    pool_size=5,
-    max_overflow=10,
+_is_sqlite = settings.database_url.startswith("sqlite")
+
+_pool_kwargs = (
+    {}
+    if _is_sqlite
+    else {"pool_pre_ping": True, "pool_size": 5, "max_overflow": 10}
 )
+
+engine = create_engine(settings.database_url, echo=False, **_pool_kwargs)
 
 
 def get_session() -> Generator[Session, None, None]:
